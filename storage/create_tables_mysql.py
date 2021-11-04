6b783dab1799ab540a27c06a02cee4965244ba46
Garvit-Kamboj <garvitkamboj1106@gmail.com>
2021-11-04 00:15:19 -0700

    Update

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 db_conn = mysql.connector.connect(
             host="acit3855-lab6a.eastus.cloudapp.azure.com", 
-            user="your_username",
-            password="your_password", 
-            database="your_database"
+            user="events",
+            password="evts", 
+            database="events"
         )
 
 db_cursor = db_conn.cursor()
